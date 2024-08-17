96b9a274d28f419d74d24e6303600acb1b181e39
Parmod Kumar <86567884+Dragcoder@users.noreply.github.com>
2024-08-17 18:10:46 +0530

    Add files via upload

--- middleware.js
+++ middleware.js
@@ -6,13 +6,9 @@ const ExpressError = require("./utils/ExpressError");
 
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){ // isAuthenticated return true/false--
-        // console.log("isAuthenticated for delete  review");
-        // console.log(req.originalUrl);
-        // console.log(res.locals);
-        // req.session.redirectUrl=req.originalUrl;
-        // console.log(req.session.redirectUrl);
+       
         let {id}=req.params; // current listingId
-        console.log("id from isLoggedIn "+id)
+       
         req.session.listingId=id;
         // console.log(req.session);
         req.flash("error","you must be logged In");
@@ -35,7 +31,7 @@ module.exports.isPostOwner=async (req,res,next)=>{
     let listing =await Listing.findById(id);
     if(!listing.owner._id.equals(res.locals.currentUser.id)){
         req.flash("error","you don't have permission to update or Delete");
-        return res.redirect(`/lists/${id}`);
+        return res.redirect(`/${id}`);
     }
     next();
 }
@@ -45,15 +41,13 @@ module.exports.isReviewAuthor=async (req,res,next)=>{
   
     // here id == [current listing id] 
     let {id,reviewId}=req.params;
-    console.log("id from isReviewAuthor",id);
-    console.log("reviewId"+reviewId);
+   
 
     let review =await Review.findById(reviewId);
-    console.log("review from isReviewAuthor"+review);
-    console.log("res.locals.currentUser._id from isReviewAuthor"+res.locals.currentUser._id)
+   
     if(!review.author.equals(res.locals.currentUser._id)){
         req.flash("error","you are not author of this review !!");
-        return res.redirect(`/lists/${id}`);
+        return res.redirect(`/${id}`);
     }
     
     next();
